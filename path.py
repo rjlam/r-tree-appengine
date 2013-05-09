e25291b5a8378de67d1e017caf1dcbbf4682b547
agorenst <agorenst@cs.wisc.edu>
2013-05-09 11:37:18 -0500

    Cleaned up code for integration with rest of r-tree.

--- path.py
+++ path.py
@@ -1,30 +1,6 @@
 import sys
 import pickle
 import DiPriQueue
-#from rTree import *
-
-# pageSize = 100
-# minEntries = 50
-# 
-# meta = DBMetaData.get_by_key_name("metadata")
-# # not initialized yet 
-# if not meta : 
-# 	tree = RTree(pageSize, minEntries)
-# 	tree.save()
-# # we already have an RTree, grab the root
-# else :
-# 	tree = RTree(None, None)
-# 
-# print "About to insert"
-# fileName = "CaStreet.ascii"
-# # the file is assumed to be of the form "x_min\ty_min\tx_max\ty_max\n"
-# insertFromTree(fileName, tree)
-# print "Done inserting"
-# 
-# searchRec = Entry(Rect([6, 4],[10,6]))
-# searchRec = Entry(Rect([-121.7,37.59], [122.2,37.8]))
-# results = tree.search(searchRec)
-# print results
 
 class Rect :
 	boundingBoxMin = []
@@ -76,6 +52,7 @@ class Rect :
 		for i in range(0, len(self.boundingBoxMin)) : 
 			self.boundingBoxMin[i] = min(self.boundingBoxMin[i],r.boundingBoxMin[i])
 			self.boundingBoxMax[i] = max(self.boundingBoxMax[i],r.boundingBoxMax[i])
+	# added to make graph work.
 	def __hash__(self):
 		        return hash((tuple(self.boundingBoxMin), tuple(self.boundingBoxMax)))
 
@@ -83,11 +60,10 @@ class Rect :
 def print_rect(r):
 	print r.boundingBoxMin, r.boundingBoxMax
 
-g = pickle.load(open('graphfile'))
-w = pickle.load(open('weightfile'))
 
 # given a rectangle s, find a rectangle that contains s
 # (in other words, s may not actually be in the R-tree, so find the ``nearest'' actual node.
+# this is yet-untested.
 def make_leaf(g, s):
     d = 1
     while len(rtree.search(s)) == 0:
@@ -100,73 +76,45 @@ def make_leaf(g, s):
 
 
 
-# for r in g[Rect([-122.259,37.8368],[-122.259,37.8374])]:
-# 	print_rect(r)
-# 	print
-# 	print "==========="
-# for r in g[Rect([-122.259,37.8362],[-122.259,37.8368])]:
-# 	print_rect(r)
-# 	print
-# print "==========="
-# for r in g[Rect([-122.259,37.8359],[-122.257,37.8362])]:
-# 	print_rect(r)
-# 	print
-# print "==========="
-# for r in g[Rect([-122.258,37.8352],[-122.257,37.8359])]:
-# 	print_rect(r)
-# 	print
-# print "==========="
-# for r in g[Rect([-122.258,37.8344],[-122.258,37.8352])]:
-# 	print_rect(r)
-# 	print
 
-
-def mindist(Q, dist):
-    distmap = [(a, dist[a]) for a in Q]
-    minpair = distmap[0]
-    for pair in distmap:
-        #print pair, minpair
-        if pair[1] < minpair[1]:
-            #print pair
-            minpair = pair
-    return minpair[0]
-
-
-
+# what's the shortest path from S to T?
+# note: S and T MUST be actual rectangles in G (ie, leaves).
+# to get S and T from arbitrary input, use "make_leaf"
 def shortest_path(graph, w, s, t):
-    print "Starting Djikstra's"
     Q = DiPriQueue.DiPriQueue()
+
+    # to get to t, first we get to prev[t]
     prev = {}
+    # surprisingly quick (~2 secs) for 20k graph.
     for v in graph:
         Q.add_task(v, float("inf"))
-        #dist[v] = float("inf")
  
-    #dist[s] = 0
     Q.add_task(s, 0)
-    #Q = list(graph.keys())[:]
 
     while len(Q) > 0:
 	udist, u = Q.pop_task()
-        #u = mindist(Q,dist)
-        #print u, Q
-        #Q.remove(u)
-        #print Q
 
+	# if we found our target we're done.
         if u == t:
             return prev
+        # if we could not find our target we're done.
         if udist == float("inf"):
             break
 
+        # for every neighbor of our next-closest node, see
+	# if that improves any of our best-paths computation.
         for v in graph[u]:
-            #alt = dist[u] + w[(u,v)]
             alt = udist + w[(u,v)]
             if alt < Q.get_dist(v):
-                #dist[v] = alt
 		Q.remove_task(v)
 		Q.add_task(v, alt)
                 prev[v] = u
+    # return the chain of links we've generated.
     return prev
 
+# work backwards from t to s.
+# the list returned is the rectangle we want to "highlight".
+# if length 1, that means we couldn't find a path.
 def extract_path(p, s, t):
     path = []
     toprint = t
@@ -181,13 +129,18 @@ def extract_path(p, s, t):
     path.append(toprint)
     return path
 
+# dead stupid.
 def print_path(p):
 	for r in p:
 		print_rect(r)
 
-#extract_path(shortest_path(test_graph, test_w, 1, 5), 1, 5)
-
-R1 = Rect([-122.259,37.8368],[-122.259,37.8374])
-R2 = Rect([-122.258,37.8344],[-122.258,37.8352])
-R3 = Rect([-121.704,37.6219],[-121.703,37.6219])
-print_path(extract_path(shortest_path(g, w, R1, R2), R1, R2))
+if __name__ == "__main__":
+	g = pickle.load(open('graphfile'))
+	w = pickle.load(open('weightfile'))
+	#some example rectangles:
+	#R1 and R2 give a nice little path, and R3 is a failure example
+	#Note: on failure, path is empty (just s).
+	R1 = Rect([-122.259,37.8368],[-122.259,37.8374])
+	R2 = Rect([-122.258,37.8344],[-122.258,37.8352])
+	R3 = Rect([-121.704,37.6219],[-121.703,37.6219])
+	print_path(extract_path(shortest_path(g, w, R1, R2), R1, R2))
